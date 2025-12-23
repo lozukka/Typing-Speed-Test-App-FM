fd008fb411f1a15d1bb073cc86e87faaa346f423
lozukka <lotsulotsu@gmail.com>
2025-12-23 07:46:03 +0200

    timer

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-console.log("hello");
 const media = window.matchMedia("(width > 700px)");
 const logoTablet = document.getElementById("logo-tablet-desktop");
 const logoMobile = document.getElementById("logo-mobile");
 const startButton = document.getElementById("start-button");
+var timeLeft = 60;
+var timerIdElement = document.getElementById("statistics-time");
+let timerInterval;
 
 function updateLogo(e) {
   const isTablet = e.matches;
@@ -19,5 +21,19 @@ function updateLogo(e) {
 
 function startTest() {
   startButton.classList.add("hidden");
+  clearInterval(timerInterval);
+  timerInterval = setInterval(countdown, 1000);
+}
+function stopTest() {
+  clearInterval(timerInterval);
+}
+
+function countdown() {
+  if (timeLeft == -1) {
+    clearTimeout(timerInterval);
+  } else {
+    timerIdElement.innerHTML = "0:" + timeLeft;
+    timeLeft--;
+  }
 }
 updateLogo(media);
